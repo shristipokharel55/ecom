c261d150dd9af37eae4cf735b453925f0ab9f4ff
shristipokharel55 <shristipokharel889@gmail.com>
2025-07-12 17:32:45 +0545

    Commenting db.js code

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,24 @@
+// Importing mongoose for MongoDB connection
 import mongoose from "mongoose"
+
+// Importing the adminSeeder function to seed default admin data after DB connection
 import { adminSeeders } from "../seeders/adminSeeders.js"
 
-
-const connectDb = async ()=>{
-
+// Asynchronous function to connect to MongoDB
+const connectDb = async () => {
     try {
+        // Attempt to connect to MongoDB using the URI from environment variables
         await mongoose.connect(process.env.MONGO_URI)
         console.log("db connected successfully")
 
+        // Seed default admin data once the connection is successful
         adminSeeders()
 
     } catch (error) {
+        // Log any error that occurs during the connection process
         console.log(error.message)
     }
 }
 
-
+// Exporting the DB connection function to use in other parts of the application
 export default connectDb;
